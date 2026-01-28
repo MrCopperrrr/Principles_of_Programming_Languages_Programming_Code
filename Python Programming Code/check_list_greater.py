1a23ff30d401d64387a3e4d65ddc62ddc26aef5f
Nguyễn Tấn Phát <ntphat0802@gmail.com>
2026-01-28 22:16:48 +0700

    Upload Python Programming Code

---
+++ Python Programming Code/check_list_greater.py
@@ -0,0 +1,5 @@
+def check(lst, n):
+    for x in lst:
+        if x <=n: 
+            return False
+    return True
---
+++ Python Programming Code/product_list.py
@@ -0,0 +1,5 @@
+def product(lst):
+    p = 1
+    for x in lst:
+        p *=x
+    return p
